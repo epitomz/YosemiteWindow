3c4acfca4b3f89302ec8206c50a6569cecf5f859
epitomz <antonin.delbos@gmail.com>
2014-12-24 10:28:04 +0100

    automatic battery % + day in time

--- js/script.js
+++ js/script.js
@@ -1,26 +1,38 @@
+/* ----------------------- AUTOMATIC TIME --------------------------- */
+
 var serverTime = new Date();
 var h = serverTime.getHours();
 var m = serverTime.getMinutes();
+var weekday = new Array(7);
+weekday[0]=  "Dim.";
+weekday[1] = "Lun.";
+weekday[2] = "Mar.";
+weekday[3] = "Mer.";
+weekday[4] = "Jeu.";
+weekday[5] = "Ven.";
+weekday[6] = "Sam.";
 
 function updateTime() {
     serverTime = new Date(serverTime.getTime() + 10000);
     h = serverTime.getHours();
     m = serverTime.getMinutes();
+    var n = weekday[serverTime.getDay()];
+    
     if (h >= 12) {
         h = h -12;
         if (m < 10) {
-            $('#time').html(h + ":0" + m + " PM");
+            $('#time').html(n + " " + h + ":0" + m + " PM");
         }
         else {
-            $('#time').html(h + ":" + m + " PM");
+            $('#time').html(n + " " + h + ":" + m + " PM");
         }
     }
     else {
         if (m < 10) {
-            $('#time').html(h + ":0" + m + " AM");
+            $('#time').html(n + " " + h + ":0" + m + " AM");
         }
         else {
-            $('#time').html(h + ":" + m + " AM");
+            $('#time').html(n + " " + h + ":" + m + " AM");
         }
     }
 }
@@ -29,3 +41,25 @@ $(function() {
     updateTime();
     setInterval(updateTime, 10000);
 });
+
+/* ----------------------- BATTERY PERCENTAGE ----------------------- */
+
+var batteryPercentage = 101;
+
+function updateBattery() {
+    batteryPercentage = batteryPercentage - 1;
+    if (batteryPercentage == 0) {
+        batteryPercentage = 100;
+        alert("Bon allez, t'as de la chance, j'ai rechargé ton mac !");
+        $('.battery-percentage').html("100%");
+    }
+    else {
+        $('.battery-percentage').html(batteryPercentage + "%");
+    }
+}
+
+$(function() {
+    updateBattery();
+    setInterval(updateBattery, 30000);
+});
+
