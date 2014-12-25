e746fedcf11de51e044b1a2f1aed5d3617429ace
epitomz <antonin.delbos@gmail.com>
2014-12-25 19:44:43 +0100

    correction id/class window + dock

--- js/script.js
+++ js/script.js
@@ -50,7 +50,7 @@ function updateBattery() {
     batteryPercentage = batteryPercentage - 1;
     if (batteryPercentage == 0) {
         batteryPercentage = 100;
-        alert("Bon allez, t'as de la chance, j'ai rechargé ton mac ! Par contre, désolé mais je n'ai pas pu remonter le temps...");
+        alert("Bon allez... T'as de la chance, j'ai rechargé ton mac ! Par contre, désolé mais je n'ai pas pu remonter le temps...");
         $('.battery-percentage').html("100%");
     }
     else {
@@ -65,26 +65,27 @@ $(function() {
 
 
 $(document).ready(function() {
-	/* ----------------------- CLOSE WINDOWS ----------------------- */
-    $( '#close' ).click(function() {
+/* -------------------------- CLOSE WINDOWS -------------------------- */
+    $( '.close' ).click(function() {
         $( this ).parents().eq(2).hide();
         var appName = $( this ).parents().eq(2).attr('id');
         $( '#' + appName + '-app' ).children( '.app-on' ).addClass( 'app-on-hidden' );
     });    
-    
+
+/* -------------------------- OPEN WINDOWS -------------------------- */    
     $( '.app' ).click(function() {
 	   var app = $(this).attr('id').slice(0, -4);
 	   $( '#' + app ).show();
     }); 
     
-    /* ------------------- HOVER BUTTONS WINDOWS -------------------- */
+/* ---------------------- HOVER BUTTONS WINDOWS ----------------------- */
 	$( '.window-button' ).mouseover(function() {
-		var name = $( this ).attr('id');
+		var name = $( this ).attr('id').slice(0, -2);
 		$( this ).attr('src', 'img/window-buttons/' + name + '-hover.png');
 	});
 	
 	$( '.window-button' ).mouseout(function() {
-		var name = $( this ).attr('id');
+		var name = $( this ).attr('id').slice(0, -2);
 		$( this ).attr('src', 'img/window-buttons/' + name + '.png');
 	});		
 	     
